68e46951643a5c0a883e57fe3fd8eed7081013a0
SOLOMON YISMAW <fantahunsolomon@gmail.com>
2020-04-16 05:49:10 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,6 +1,11 @@
 $status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-def mario
 
+def mario
   phrase = "It's-a me, Mario!"
   puts phrase
+end
+
+
+def toadstool
+  puts $status
 end
